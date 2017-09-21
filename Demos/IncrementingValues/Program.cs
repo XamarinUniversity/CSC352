abf13d1c5529fa619735a0e41f5285ae6adffd7f
Adam Patridge <patridge@gmail.com>
2017-09-21 14:31:54 -0600

    Add files missed by migration.

---
+++ Demos/IncrementingValues/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace IncrementingValues
+{
+    public class Counter
+    {
+        int counter;
+        public int Value => counter;
+
+        public virtual void Increment() { 
+            counter++; 
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Counter c = new Counter();
+
+            // Increment counter 10k times with two threads.
+            var t = Enumerable.Range(0,2)
+                .Select(n => Task.Run(() => {
+                    for (int i = 0; i < 5000; i++)
+                        c.Increment();
+                })).ToArray();
+
+            // Wait for both to be finished.
+            Task.WaitAll(t);
+
+            // Output the result.
+            Console.WriteLine($"Final value is {c.Value}.");
+        }
+    }
+}
---
+++ Demos/VolatileLooper/VolatileLooper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Volatile.Looper
+{
+    class Program
+    {
+        static bool terminate = false;
+        public static void Main()
+        {
+            var t = Task.Run(() =>
+            {
+                Console.WriteLine( "Starting counter." );
+                long counter = 0;
+                while (!terminate)
+                    counter++;
+                Console.WriteLine( "Loop ended at: " + counter );
+            });
+
+            // Wait 2 seconds.
+            Task.Delay(2000).Wait();
+
+            Console.WriteLine("Signaling termination.");
+
+            terminate = true;
+            t.Wait();
+
+            Console.WriteLine("Done, program ending.");
+        }
+    }
+}
